c8db3bcd05c1736cac1631c3b32c7596492a4eb5
nour_morsy <nourmorse21@gmail.com>
2023-10-13 19:05:35 +0300

    docker_file

---
+++ src/catkin_ws/build/hello/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/developer/workspace/src/catkin_ws/src/hello
+# Build directory: /workspace/src/catkin_ws/build/hello
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
